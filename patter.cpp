8efbde861ed955d6d22a2ed76e06d9768064df0e
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2025-06-26 21:32:22 +0530

    changes

--- patter.cpp
+++ patter.cpp
@@ -284,21 +284,25 @@ void print16_1(int n = 5){
     }
 }
 void print17(int n = 5){
-     //symmety
-        int spaces = (2*n)-2;
-    for( int i = 1; i<=n; i++){
-        for(int j = 1; j<=i; j++){
+    int space = 2*n-2;
+      for(int i = 1; i<=2*n-1; i++)
+    {
+        int stars = i;
+        if(i>n) stars = 2*n-i;
+        for(int j = 1; j<=stars; j++)
+        {
             cout<<"*";
         }
-        for(int j = 0; j<spaces; j++){
+        for(int j = 1; j<=space; j++){
             cout<<" ";
         }
-        for(int j = i; j>=1; j--){
+        for(int j = 1; j<=stars; j++)
+        {
             cout<<"*";
         }
         cout<<endl;
-        if(i <= 2*n-1/2) spaces += 2;
-        spaces -= 2;
+        if(i < n ) space -= 2;
+        else space += 2;
     }
 }
 int main()
