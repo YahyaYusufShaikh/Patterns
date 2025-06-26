7327b9115965ac548fc7c38951c28f7568c57e6f
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2025-06-26 21:40:36 +0530

    changes made

--- patter.cpp
+++ patter.cpp
@@ -284,26 +284,28 @@ void print16_1(int n = 5){
     }
 }
 void print17(int n = 5){
-    int space = 2*n-2;
-      for(int i = 1; i<=2*n-1; i++)
-    {
+    int space = 2*n - 2;
+    for(int i = 1; i<=2*n-1; i++){
         int stars = i;
-        if(i>n) stars = 2*n-i;
-        for(int j = 1; j<=stars; j++)
-        {
+        if(i > n) stars = 2*n - i;
+        //stars
+        for(int j = 1; j<=stars; j++){
             cout<<"*";
         }
+        //spaces
         for(int j = 1; j<=space; j++){
             cout<<" ";
         }
-        for(int j = 1; j<=stars; j++)
-        {
+        for(int j = 1; j<=stars; j++){
             cout<<"*";
         }
         cout<<endl;
-        if(i < n ) space -= 2;
+        if(i < n) space -= 2;
         else space += 2;
+
+
     }
+
 }
 int main()
 {
