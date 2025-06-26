fa013d14cfb8a0c55218f69564d47ba4266e0b33
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2025-06-26 09:08:01 +0530

    char added

--- patter.cpp
+++ patter.cpp
@@ -208,17 +208,18 @@ void print13(int n = 5){
     }
 }
 void print14(int n = 5){
-    for(int i = 0; i<=n; i++){
+    for(int i = 0; i<n; i++){
         // spaces
-        for(int j = 0; j<=n-i-1; j++){
+        for(int j = 0; j<n-i-1; j++){
             cout<<" ";
         }
         // character
-        for(char j = 0; j<2*i-1; j++){
-            cout<<"*";
+        char c = 'A';
+        for(char j = 0; j<2*i+1; j++){
+            cout<<c;
         }
         // spaces
-        for(int j = 0; j<=n-i-1; j++){
+        for(int j = 0; j<n-i-1; j++){
             cout<<" ";
         }
         cout<<endl;
